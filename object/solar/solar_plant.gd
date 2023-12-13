fdeadfb333c0af51c0f1943a831b19d5a2042d45
phelantomas <phelantomas@gmail.com>
2023-12-13 20:03:25 +0100

    fix bugs

--- object/solar/solar_plant.gd
+++ object/solar/solar_plant.gd
@@ -4,9 +4,6 @@ var _speed: float = 1.0
 var _target_output: float = 1.0
 var _variance: float = 0.1 # Represents random production deviation per windmill
 
-func _init() -> void:
-	add_to_group(Constants.GROUP_PRODUCER)
-
 func _physics_process(delta: float) -> void:
 	_target_output = clampf(Weather.sun - _variance, 0.0, 1.0)
 	active_capability_out = _target_output * nominal_power
