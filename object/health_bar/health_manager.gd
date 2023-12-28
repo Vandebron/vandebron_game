4aafdeef776cba8900605fa2a85fa4cf4117f04f
Dick Visser <dickvisser1994@gmail.com>
2023-12-28 21:54:38 +0100

    Update health at set interval

--- object/health_bar/health_manager.gd
+++ object/health_bar/health_manager.gd
@@ -6,16 +6,26 @@ signal health_zeroed
 @export var energy_grid: EnergyGrid
 @export var heal_factor: float = 0.01
 @export var hurt_factor: float = 0.01
+@export var update_interval_ms: int = 100
 
 var health: float = 50.0
 var _done: bool
+var _update_timer: Timer
 
 
-func _physics_process(_delta: float) -> void:
+func _ready() -> void:
+	_update_timer = Timer.new()
+	_update_timer.wait_time = update_interval_ms / 1000.0
+	_update_timer.timeout.connect(update)
+	add_child(_update_timer)
+	_update_timer.start()
+
+
+func update() -> void:
 	if _done:
 		return # Hack - so we don't keep spamming the signal
 	
-	var balance_diff: float = abs(energy_grid.BALANCE_CENTER - energy_grid.balance)
+	var balance_diff: float = absf(energy_grid.BALANCE_CENTER - energy_grid.balance)
 	var balance_diff_hz: float = balance_diff * energy_grid.target_frequency_hz
 	var deviation: float = balance_diff_hz / energy_grid.frequency_max_deviation_hz
 	
