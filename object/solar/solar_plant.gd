17bc4a9dd1ba66053358a7292e22dd695e51f38c
Dick Visser <dickvisser1994@gmail.com>
2023-12-29 12:04:20 +0100

    Do minor formatting

--- object/solar/solar_plant.gd
+++ object/solar/solar_plant.gd
@@ -2,12 +2,13 @@ extends Producer
 
 
 func _physics_process(_delta: float) -> void:
-	if (weather.is_night()):
-		active_capability_out = 0
-		current_power = 0
+	if weather.is_night():
+		active_capability_out = 0.0
+		current_power = 0.0
 	else:
-		active_capability_out = calculate_power_output(weather._get_sun_strength())
+		active_capability_out = calculate_power_output(weather.sun_strength)
 		current_power = clampf(active_capability_out, dmol, nominal_power)
 	
+	
 func calculate_power_output(point_of_day: float) -> float:
-	return lerp(0.0, nominal_power, point_of_day)
+	return lerpf(0.0, nominal_power, point_of_day)
