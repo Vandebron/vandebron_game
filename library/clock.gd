f07d6f4ae9d29bccdf4221d36494f10dd820b3c4
Dick Visser <dickvisser1994@gmail.com>
2024-04-05 13:18:56 +0200

    Long daynight cycle; start game in summer

--- library/clock.gd
+++ library/clock.gd
@@ -5,12 +5,12 @@ signal part_of_day_ended(part: Clock.DayPart)
 signal season_started(season: Clock.Season)
 signal season_ended(season: Clock.Season)
 
-enum Season {AUTUMN, WINTER, SPRING, SUMMER}
+enum Season {SUMMER, AUTUMN, WINTER, SPRING}
 enum DayPart {DAWN, NOON, DUSK, NIGHT}
 
 @export var days_per_season: int = 7
 # Cycle of 30 seconds means day=15s, night=15s
-@export var day_night_cycle_ms: int = 30_000
+@export var day_night_cycle_ms: int = 60_000
 # Start the game at 6 'o clock
 @export var game_start_offset_ms: int = floori((6.0 / 24.0) * float(day_night_cycle_ms))
 
@@ -31,8 +31,10 @@ func _physics_process(_delta: float) -> void:
 	point_of_day = _get_point_of_day() # This gets sampled so much, so we cache it here
 	_handle_day_transitions()
 
+
 func _process(delta: float) -> void:
-	current_tick_ms += int(delta * 1000)
+	current_tick_ms += int(delta * 1000.0)
+
 
 func is_dawn() -> bool:
 	return part_of_day == DayPart.DAWN
@@ -57,21 +59,25 @@ func is_day() -> bool:
 ## Returns value in 0-24h range
 func _get_time_of_day() -> float:
 	return point_of_day * 24.0
-	
+
+
 ## Returns value in 0-24h range for a given point in the future
 func get_part_of_day_plus(offset_time_ms: int) -> float:
 	var t: float = _get_cycle_value(int(day_night_cycle_ms), game_start_offset_ms + offset_time_ms)
 	var point_of_day_in_future = (1.0 - t) / 2.0
 	return point_of_day_in_future * 24.0
-	
+
+
 func _get_cycle_value(cycle_time_ms: int, offset: int = 0) -> float:
 	var current_time_ms: int = current_tick_ms + offset
 	return 1.0 - (2.0 * (current_time_ms % cycle_time_ms)) / cycle_time_ms
+
 
 ## Returns value in 0-1 range.
 func _get_point_of_day() -> float:
 	var t: float = _get_cycle_value(int(day_night_cycle_ms), game_start_offset_ms)
 	return (1.0 - t) / 2.0
+
 
 func _get_part_of_day() -> DayPart:
 	var time: float = time_of_day
@@ -82,6 +88,7 @@ func _get_part_of_day() -> DayPart:
 	if time > 18.00 && time <= 24.00:
 		return DayPart.DUSK
 	return DayPart.NIGHT
+
 
 func _handle_day_transitions() -> void:
 	if part_of_day == _prev_part_of_day:
