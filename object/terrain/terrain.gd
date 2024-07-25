d06b479b5ea8b0c12c790ab880a4e9501067e3a8
Dick Visser <dickvisser1994@gmail.com>
2024-07-25 09:31:30 +0200

    Improve terrain look with a secondary color

--- object/terrain/terrain.gd
+++ object/terrain/terrain.gd
@@ -33,36 +33,48 @@ func _process(_delta: float) -> void:
 func _on_season_started(season: Clock.Season) -> void:
 	match season:
 		Clock.Season.AUTUMN:
-			_set_ground_color(Color.hex(0xd3a068ff))
+			_set_main_color(Color.hex(0xd3a068ff))
+			_set_secondary_color(Color.hex(0xc08e56ff))
+			_set_splatmap_edge(0.3)
 			TREE_LEAVES_MAT.set_shader_parameter("albedo", Color.hex(0xa77b5bff))
 		Clock.Season.WINTER:
-			_set_snow_density(0.65)
-			_set_ground_color(Color.hex(0xa77b5bff))
+			_set_main_color(Color.hex(0xa77b5bff))
+			_set_secondary_color(Color.hex(0xffffffff))
+			_set_splatmap_edge(0.65)
 			_set_tree_leaves_transparency(1.0)
 		Clock.Season.SPRING:
+			_set_main_color(Color.hex(0xc2d368ff))
+			_set_secondary_color(Color.hex(0x94b250ff))
+			_set_splatmap_edge(0.75)
 			TREE_LEAVES_MAT.set_shader_parameter("albedo", Color.hex(0x8ab060ff))
-			_set_ground_color(Color.hex(0xc2d368ff))
 		Clock.Season.SUMMER:
-			_set_ground_color(Color.hex(0x8ab060ff))
+			_set_main_color(Color.hex(0x8ab060ff))
+			_set_secondary_color(Color.hex(0x779a50ff))
+			_set_splatmap_edge(0.5)
 
 
 func _on_season_ended(season: Clock.Season) -> void:
 	match season:
 		Clock.Season.WINTER:
-			_set_snow_density(0.0)
 			_set_tree_leaves_transparency(0.0)
 
 
-func _set_ground_color(color: Color) -> void:
+func _set_main_color(color: Color) -> void:
 	var tween: Tween = create_tween()
 	tween.set_trans(Tween.TRANS_ELASTIC)
-	tween.tween_property(terrain_material, "shader_parameter/ground_color", color, 2.0)
+	tween.tween_property(terrain_material, "shader_parameter/main_color", color, 2.0)
 
 
-func _set_snow_density(density: float) -> void:
+func _set_secondary_color(color: Color) -> void:
 	var tween: Tween = create_tween()
 	tween.set_trans(Tween.TRANS_ELASTIC)
-	tween.tween_property(terrain_material, "shader_parameter/snow_density", density, 2.0)
+	tween.tween_property(terrain_material, "shader_parameter/second_color", color, 2.0)
+
+
+func _set_splatmap_edge(density: float) -> void:
+	var tween: Tween = create_tween()
+	tween.set_trans(Tween.TRANS_ELASTIC)
+	tween.tween_property(terrain_material, "shader_parameter/edge", density, 2.0)
 
 
 func _set_tree_leaves_transparency(transparency: float) -> void:
